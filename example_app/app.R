c74f97ae130814cf0a8bfa6e38c9959298734828
katelh4 <kate.hutchinson@defra.gov.uk>
2024-04-04 16:32:05 +0000

    added more comments

--- example_app/app.R
+++ example_app/app.R
@@ -6,7 +6,8 @@ library(DT)
 source("R/get_data.R")
 source("R/utils.R")
 
-# Define UI for application that draws a histogram
+# Define UI - the UI (user interface) controls the layout and appearance 
+# of your app (the front end)
 ui <- fluidPage(
   
   # create navigation bar for page
@@ -19,17 +20,26 @@ ui <- fluidPage(
              # side bar for user input
              sidebarLayout(
                sidebarPanel(
-                 # dropdown to choose the year, populated by the years in the dataset
+                 # dropdown to choose the year, populated by the years in the dataset:
+                 # the first argument here "year" is the id, which is how we will 
+                 # reference this input in the server function
+                 # the label just gives a title above the dropdown
+                 # the choices are what will be listed in the dropdown, so we 
+                 # give it the list of years in the dataset
                  selectInput(
                    "year", label = "Year", choices = unique(data$year)
                  )),
 
                # main panel to display outputs
                mainPanel(
-                 # output - tabset with plot and table
+                 # output - display this in tabset panels
                  tabsetPanel(
+                   # the first tab will have the title "Plot" and display 
+                   # a plotly chart
                    tabPanel("Plot", plotlyOutput("plot")),
-                   tabPanel("Table", dataTableOutput("table"))
+                   # the second tab will have the title "Data Table" and display
+                   # a data table
+                   tabPanel("Data Table", dataTableOutput("table"))
                  ))
              )),
 
@@ -45,19 +55,22 @@ ui <- fluidPage(
 
   ))
 
-# Define server logic required to draw a histogram
+# Define server - this contains the instructions needed to build the app (the back end)
 server <- function(input, output) {
   
   # set the year for the whole page based on what is selected in the dropdown
-  # this makes selected_year a reactive expression, so from here we reference it like a function as selected_year()
+  # this makes selected_year a reactive expression
+  # so from here we reference it like a function as selected_year()
+  # the input$year comes from the id we set in the ui function
   selected_year <- reactive({
     input$year
   })
 
   # generate a plot of the data for selected year
+  # output$plot because we set the plotly output id to "plot" in the ui function
   output$plot <- renderPlotly({
 
-    # filter to the selected year
+    # filter data to the selected year
     plot_data <- data %>% filter(year == selected_year())
 
     # create plotly plot
