6539767d510cf9ac6b1b5cacec9a944d4939d240
Kate Hutchinson <kate.hutchinson@defra.gov.uk>
2024-08-22 13:44:36 +0000

    updates to make it templatey

--- example_app/R/utils.R
+++ example_app/R/utils.R
@@ -1,20 +1,17 @@
 # Functions that will be used in the app
 
 # scatter chart for bill vs flipper length
-bill_flipper_plot <- function(data, species){
-  
-  # filter data to selected species
-  plot_data <- data %>% filter(species == species)
+bill_flipper_plot <- function(data){
   
   # create plot
   fig <- plot_ly(
-    data = plot_data,
+    data = data,
     x = ~bill_length_mm,
     y = ~flipper_length_mm,
-    color = ~island,
+    color = ~species,
     colors = c("#12436D", "#28A197", "#801650"),
     type = "scatter",
-    text = ~island,
+    text = ~species,
     mode = "markers",
     # set the text to display in the hover over
     hovertemplate = paste0(
@@ -30,7 +27,7 @@ bill_flipper_plot <- function(data, species){
                    showgrid = FALSE),
       yaxis = list(title = "Flipper length (mm)",
                    range = c(155, 240)),
-      legend = list(title = list(text = "<b>Island</b>"))
+      legend = list(title = list(text = "<b>Species</b>"))
     )
   
   return(fig)
--- example_app/app.R
+++ example_app/app.R
@@ -4,6 +4,10 @@ library(plotly)
 library(dplyr)
 library(DT)
 library(shinydashboard)
+library(reactable)
+library(shinyfilter)
+library(shinyWidgets)
+library(datamods)
 
 # Load functions and data from scripts in the R folder
 source("R/get_data.R")
@@ -17,58 +21,119 @@ ui <- dashboardPage(
   skin = "green",
   
   # Create header - set title of the app 
-  dashboardHeader(title = "Penguins"),
+  dashboardHeader(title = "RShiny Template"),
   
   # Create sidebar
   dashboardSidebar(
-    # Each menuItem is like a tab and will display different content in the main body
     sidebarMenu(
-      menuItem("Adelie", tabName = "adelie"),
-      menuItem("Gentoo", tabName = "gentoo"),
-      menuItem("Chinstrap", tabName = "chinstrap"),
-      # Inputs can also go in the sidebar
-      # Create a dropdown input for the year, populated by the years in the dataset
-      selectInput(
-        inputId = "year", label = "Year", choices = unique(data$year)
-      )
+      # add an image at the top of the sidebar - good place for a logo
+      div(
+        style = "margin:10px 40px 10px 40px;",
+        img(
+          src = "Images/DASH_logo_white.png",
+          width = "148",
+          height = "136"
+        )
+      ),
+      # add a break between the image and menu
+      br(),
+      # Each menuItem is like a tab and will display different content in the main body
+      # the tabName is like an ID for the page to refer to it later in the script
+      menuItem("General Stats", tabName = "stats"),
+      menuItem("Dataset", tabName = "dataset"),
+      menuItem("About", tabName = "about")
     )
   ),
   
   # Create main body 
   dashboardBody(
+    # this is a useful bit of html which freezes the header and sidebar on the page
+    tags$script(HTML(
+      "$('body').addClass('fixed');"
+    )),
     # Create the contents of each sidebar tab
     tabItems(
-      # Use the tabName of the relevant sidebar menuItem 
-      tabItem(tabName = "adelie", 
-              # Create a row on the page
+      # Use the tabName of the relevant sidebar menuItem
+      tabItem(
+        tabName = "stats",
+        # Create a row on the page
+        fluidRow(
+          # put content into a box - boxes can be used to split up the contents of the page
+          box(
+            # Create a dropdown input for the island, populated by the dataset
+            selectInput(
+              inputId = "island",
+              label = "Island",
+              choices = unique(data$island)
+            )
+          )),
+        fluidRow(
+          # Create value boxes (the full page width is 12)
+          valueBoxOutput(outputId = "adelie_box",
+                         width = 4),
+          valueBoxOutput(outputId = "chinstrap_box",
+                         width = 4),
+          valueBoxOutput(outputId = "gentoo_box",
+                         width = 4)
+        ),
+        
+        fluidRow(
+          # Create a box with tabs to display different content
+          tabBox(
+            width = 12,
+            # The first tab will display the plotly chart
+            tabPanel(title = "Chart",
+                     plotlyOutput("plot")),
+            # The second tab will display the table
+            tabPanel(
+              title = "Table",
+              dataTableOutput("table"),
+              downloadButton(outputId = "downloadData",
+                             label = "Download")
+            )
+          ))
+      ),
+      # Tab for displaying and filtering the dataset
+      tabItem(tabName = "dataset",
               fluidRow(
-                # Create value boxes (the full page width is 12)
-                valueBoxOutput(outputId = "biscoe_box", 
-                               width = 4),
-                valueBoxOutput(outputId = "dream_box", 
-                               width = 4),
-                valueBoxOutput(outputId = "torgersen_box", 
-                               width = 4)
-              ),
-              
-              fluidRow(
-                # Create a box with tabs to display different content
-                tabBox(width = 12, 
-                       # The first tab will display the plotly chart
-                       tabPanel(title = "Chart",
-                                plotlyOutput("plot")),
-                       # The second tab will display the table
-                       tabPanel(title = "Table",
-                                dataTableOutput("table"),
-                                downloadButton(outputId = "downloadData", 
-                                               label = "Download"))
-                       )
+                box(title = "Apply filters",
+                      # set up filters
+                      select_group_ui(
+                        id = "my-filters",
+                        params = list(
+                          species = list(inputId = "species",
+                                         label = "Species"),
+                          island = list(inputId = "island",
+                                        label = "Island"),
+                          sex = list(inputId = "sex",
+                                     label = "Sex"),
+                          year = list(inputId = "year",
+                                      label = "Year")
+                        )
+                      ),
+                    width = 12),
+                # download button
+                fluidRow(box(
+                  title = "Download",
+                  actionButton("Download", "Download data",
+                               onClick = "Reactable.downloadDataCSV('data', 'penguins.csv')"),
+                  width = 12
                 )),
-      # Second and third tabs for the other penguin species 
-      tabItem(tabName = "gentoo"),
-      tabItem(tabName = "chinstrap")
-      )
-    )
+                # data table
+                fluidRow(box(
+                  title = "Penguins dataset",
+                  reactableOutput(outputId = "tbl_penguins"),
+                  width = 12
+                ))
+              )),
+      # Tab for general information
+      tabItem(tabName = "about",
+              fluidRow(
+                box(title = "About",
+                    "Information about your app",
+                    width = 12)
+              ))
+    ))
 )
 
 # Define server - this contains the instructions needed to build the app (the back end)
@@ -78,13 +143,13 @@ server <- function(input, output) {
   # this makes selected_year a reactive expression
   # so from here we reference it like a function: selected_year()
   # the input$year comes from the id we set in the ui function
-  selected_year <- reactive({
-    input$year
+  selected_island <- reactive({
+    input$island
   })
   
   # Filter the dataset in a reactive expression too
   filtered_data <- reactive({
-    data %>% filter(year == selected_year() & species == "Adelie")
+    data %>% filter(island == selected_island())
   })
   
   # Generate a plot of the data for selected year
@@ -92,7 +157,7 @@ server <- function(input, output) {
   output$plot <- renderPlotly({
     
     # Create plotly plot using a function created in R/utils.R
-    bill_flipper_plot(filtered_data(), species = "Adelie")
+    bill_flipper_plot(filtered_data())
     
   })
   
@@ -112,22 +177,41 @@ server <- function(input, output) {
   # Generate the value boxes - set the values based on the filtered data 
   # and the appearance of the box
   
-  output$biscoe_box <- renderValueBox({
-    valueBox(subtitle = "Biscoe",
-             value = filtered_data() %>% filter(island == "Biscoe") %>% nrow(),
+  output$adelie_box <- renderValueBox({
+    valueBox(subtitle = "Adelie",
+             value = filtered_data() %>% filter(species == "Adelie") %>% nrow(),
              color = "blue")
   })
   
-  output$dream_box <- renderValueBox({
-    valueBox(subtitle = "Dream",
-             value = filtered_data() %>% filter(island == "Dream") %>% nrow(),
+  output$gentoo_box <- renderValueBox({
+    valueBox(subtitle = "Gentoo",
+             value = filtered_data() %>% filter(species == "Gentoo") %>% nrow(),
              color = "purple")
   })
  
-  output$torgersen_box <- renderValueBox({
-    valueBox(subtitle = "Torgersen",
-             value = filtered_data() %>% filter(island == "Torgersen") %>% nrow(),
+  output$chinstrap_box <- renderValueBox({
+    valueBox(subtitle = "Chinstrap",
+             value = filtered_data() %>% filter(species == "Chinstrap") %>% nrow(),
              color = "olive")
+  })
+  
+  # set up table with filters
+  res_mod <- select_group_server(id = "my-filters",
+                                 data = reactive(data),
+                                 vars = reactive(
+                                   c(
+                                     "species",
+                                     "island",
+                                     "sex",
+                                     "year"
+                                   )
+                                 ))
+  
+  # DASH register table
+  output$tbl_penguins <- renderReactable({
+    reactable(res_mod(),
+              searchable = TRUE,
+              highlight = TRUE)
   })
   
 }
